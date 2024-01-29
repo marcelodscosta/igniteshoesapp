bb5de6ceb4c1758a14b7a991a4b41ea3c26c9742
Marcelo Costa <marcelodscosta@yahoo.com.br>
2024-01-28 22:25:46 -0300

    onesignal initial settings

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import { StatusBar } from 'react-native';
+import OneSignal from 'react-native-onesignal';
+
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
 import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { Routes } from './src/routes';
 
-import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
+import { THEME } from './src/theme';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.setAppId('528abd34-dc31-4da8-b134-d4e7f255aa2c');
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
