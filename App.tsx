7aca5a3532c139a8a69e80fdbb05e0ae4d90be39
Marcelo Costa <marcelodscosta@yahoo.com.br>
2024-01-29 02:19:57 -0300

    display notification when app is open

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { StatusBar } from 'react-native';
-import OneSignal, { NotificationReceivedEvent } from 'react-native-onesignal';
+import OneSignal, { NotificationReceivedEvent, OSNotification } from 'react-native-onesignal';
 
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
 import { NativeBaseProvider } from 'native-base';
@@ -11,6 +11,7 @@ import { Routes } from './src/routes';
 import { Loading } from './src/components/Loading';
 import { THEME } from './src/theme';
 
+import { Notification } from './src/components/Notification';
 import { CartContextProvider } from './src/contexts/CartContext';
 import { tagUserInfoCreate } from './src/notifications/notificationsTags';
 
@@ -18,13 +19,15 @@ OneSignal.setAppId('528abd34-dc31-4da8-b134-d4e7f255aa2c');
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  const [notification, setNotification] = useState<OSNotification>();
 
   tagUserInfoCreate();
 
   useEffect(() => {
     const unsubscribe = OneSignal
       .setNotificationWillShowInForegroundHandler((notificationReceivedEvent: NotificationReceivedEvent) => {
-        console.log(notificationReceivedEvent);
+        const response = notificationReceivedEvent.getNotification();
+        setNotification(response);
 
       });
   }, [])
@@ -40,6 +43,13 @@ export default function App() {
       <CartContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
       </CartContextProvider>
+      {
+        notification?.title &&
+        <Notification
+          title={notification.title}
+          onClose={() => { setNotification(undefined) }}
+        />
+      }
     </NativeBaseProvider>
   );
 }
