0c1a0f5ff0cf3a69daebdd2c67adce4b331c8264
Marcelo Costa <marcelodscosta@yahoo.com.br>
2024-01-29 07:38:56 -0300

    passing notifications in the foreground

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,19 +1,30 @@
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { CloseIcon, HStack, Icon, IconButton, Pressable, Text } from 'native-base';
+import { OSNotification } from 'react-native-onesignal';
 
 type Props = {
-  title: string;
+  data: OSNotification;
   onClose: () => void;
 }
 
-export function Notification({ title, onClose }: Props) {
+type AdditionalDataProps = {
+  route?: 'details';
+  product_id?: string;
+};
+
+export function Notification({ data, onClose }: Props) {
 
   const { navigate } = useNavigation();
 
   const handleOnPress = () => {
-    navigate('details', { productId: '7' })
-    onClose();
+    const { route, product_id } = data.additionalData as AdditionalDataProps;
+
+    if (route === 'details' && product_id) {
+      navigate('details', { productId: product_id })
+      onClose();
+
+    }
   };
 
   return (
@@ -33,7 +44,7 @@ export function Notification({ title, onClose }: Props) {
         <Icon as={Ionicons} name="notifications-outline" size={5} color="black" mr={2} />
 
         <Text fontSize="md" color="black" flex={1}>
-          {title}
+          {data.title}
         </Text>
 
         <IconButton
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -30,7 +30,7 @@ export function Routes() {
       {
         notification?.title &&
         <Notification
-          title={notification.title}
+          data={notification}
           onClose={() => { setNotification(undefined) }}
         />
       }
